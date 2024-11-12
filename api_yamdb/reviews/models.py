333dd555bc2221b1a6608cf4a067fb0715883dfd
Valeriy Smirnov <valeriy.5mirnov@yandex.ru>
2024-11-12 20:46:35 +0700

    add models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,5 +2,76 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+SYMBOLS_LIMIT = 25
+
+
 class User(AbstractUser):
     role = models.CharField("роль", max_length=20, blank=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Слаг')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Слаг')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    year = models.SmallIntegerField(verbose_name='Год создания')
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='genres',
+        through='TitleGenres',
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='categories',
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return f'{self.name}, {self.year}, {self.description[:SYMBOLS_LIMIT]}'
+
+
+class TitleGenres(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
