8a47c61dc146db2214875efd116b84ed3777205e
cryosteam <g.korenblyum@gmail.com>
2024-11-13 18:03:03 +0000

    feat: add user signup and token

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,5 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-UserAdmin.fieldsets += (("Extra Fields", {"fields": ("role",)}),)
+UserAdmin.fieldsets += (
+    ('Extra Fields', {'fields': ('role', 'confirmation_code')}),
+)
 admin.site.register(User, UserAdmin)
