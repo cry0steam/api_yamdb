1cc5f7cca25a5d31df58ffe1e4652c03e458caa8
Denis Bessonov <denibessono@yandex.ru>
2024-11-14 19:27:24 +0300

    Изменение формата поля pub_date модели Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -109,7 +109,7 @@ class Review(models.Model):
         Title, on_delete=models.CASCADE, related_name='reviews'
     )
     text = models.TextField()
-    pub_date = models.DateField(
+    pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
     score = models.IntegerField(
